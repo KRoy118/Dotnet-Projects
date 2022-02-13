4bd7b5a8b1fa69edd33c66f14400c131efccf1bf
Katha Roy <55144881+KRoy118@users.noreply.github.com>
2022-02-13 12:12:20 +0530

    Initial commit

---
+++ CountOfEevenNumbrs/Program.cs
@@ -0,0 +1,29 @@
+﻿// Count of even numbers present in entered array
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Solution
+{
+ class Program
+ {
+ public static void Main()
+ {int i;
+ int n=Convert.ToInt32(Console.ReadLine());
+ int [] ar=new int[n];
+ for( i=0;i<n;i++)
+ { ar[i]=Convert.ToInt32(Console.ReadLine());
+   
+ }
+ int c=0;
+ for(i=0;i<ar.Length;i++)
+ {
+     if(ar[i]%2==0)
+     c ++ ;
+ }
+ Console.WriteLine(c);
+
+ }
+
+}
+}
---
+++ RemoveSpecifiedCharFrmStr/Program.cs
@@ -0,0 +1,22 @@
+﻿//Remove specified a character from a non-empty string using index of a character
+//Space  NOT counted as a char
+using System;
+
+namespace RemoveSpecifiedCharFrmStr
+{
+    class Program
+    {int i;
+        static void Main(string[] args)
+        {   Console.Write("Enter the string: ");
+            string str=Console.ReadLine();
+            Console.Write("Enter the character order to be removed: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            //string str=str.Replace(" ","");
+           // Console.WriteLine(str1);
+            Console.Write("Output type1:");
+            Console.WriteLine(str.Remove((n-1),1));
+            Console.Write("Output type2:");
+            Console.WriteLine(str.Replace( str[n-1],'\0') );
+        }
+    }
+}
